e7e901c1c16bc8243ca35727bb22346e927cd160
Student User <student@workstation.lab.example.com>
2022-07-20 11:26:16 -0400

    one more time

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,8 +1,6 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is coming!\n";
-print "\n";
+print "Hello, World! php version is " . PHP_VERSION . "<br/>";
+print "A change is coming!<br/>";
 echo(rand(10, 100));
-print "\n";
-print "Name: Williams";
+print "<br>Name: Williams";
 ?>
